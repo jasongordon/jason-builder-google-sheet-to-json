6233baf0f3642543f0920a3578e41bcafb0a1dac
jasongordon <github@jasongordon.com>
2018-06-18 20:17:53 -0400

    Added new code to clean variables values and add values that are missing

--- test.js
+++ test.js
@@ -1,15 +1,32 @@
 var gsjson = require('google-spreadsheet-to-json');
 gsjson({
-	spreadsheetId: '1X_P3wzJTSF0K36kx1oxa3BHEnz8TnvVKLQLXrR4BCXc',
+	spreadsheetId: '1iaanivIVtNDMVFQ31b6tl5C8ZaubqBBRgZLjr-ewE6Y',
 	ignoreRow: 1,
 	propertyMode: "nospace",
 	beautify: 1
 }).then(function(result) {
 	result.forEach(function(part, index, theArray) {
   		var t = theArray[index] ;
-		t['front'] = "{{ '" + t['front'] + "' | asset_url }}";
-		t['top'] = "{{ '" + t['top'] + "' | asset_url }}";
-		t['isomatric']  = "{{ '" + t['isometric'] + "' | asset_url }}";
+		if(t['front'] !== undefined)
+			t['front'] = "{{ '" + t['front'] + "' | asset_url }}";
+		else
+			t['front'] = '';
+		if(t['top'] !== undefined)
+			t['top'] = "{{ '" + t['top'] + "' | asset_url }}";
+		else
+			t['top'] = '';
+		if(t['isometric'] !== undefined)
+			t['isometric']  = "{{ '" + t['isometric'] + "' | asset_url }}";
+		else
+			t['isometric'] = '';
+		if(t['frontKerning'] === undefined)
+			t['frontKerning'] = '';
+		if(t['topKerning'] === undefined)
+			t['topKerning'] = '';
+		if(t['isometricKerning'] === undefined)
+			t['isometricKerning'] = '';
+		if(t['name'] !== undefined) // delete unused name column
+			delete t['name'];
 		theArray[index] = t; 
 	});
 	result = JSON.stringify(result, null, 4 )  
